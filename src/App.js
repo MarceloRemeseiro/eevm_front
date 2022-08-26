1c9e45da56a420de03a1225142b0bbf88f1537c5
Marcelo Remeseiro <marcelo.remeseiro@gmail.com>
2022-08-26 03:15:16 +0200

    spinner

--- src/App.js
+++ src/App.js
@@ -34,51 +34,71 @@ export function App() {
   };
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter className="App">
-        <ScrollToTop />
-        <div>
-          <Top />
+    <>
+      <QueryClientProvider client={queryClient}><Top />
+        <BrowserRouter className="App">
+          <ScrollToTop />
+          
           <Sidebar isOpen={isOpen} toggle={toggle} />
           <Navbar2 toggle={toggle} />
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="actividad" element={<Actividad />}></Route>
-            <Route path="media" element={<Media />}></Route>
-            <Route path="misionVision" element={<MisionVision />}></Route>
-            <Route path="queCreemos" element={<QueCreemos />}></Route>
-            <Route path="jesus" element={<Jesus />}></Route>
-            <Route path="pastores" element={<Pastores />}></Route>
-            <Route path="contacto" element={<Contacto />}></Route>
-            <Route path="dar" element={<Dar />}></Route>
-            <Route path="actividad/actividad1" element={<Actividad1 />}></Route>
-            <Route path="actividad/actividad2" element={<Actividad2 />}></Route>
-            <Route path="actividad/actividad3" element={<Actividad3 />}></Route>
-            <Route path="actividad/actividad4" element={<Actividad4 />}></Route>
-            <Route path="actividad/actividad5" element={<Actividad5 />}></Route>
-            <Route path="actividad/actividad6" element={<Actividad6 />}></Route>
-            <Route path="actividad1" element={<Actividad1 />}></Route>
-            <Route path="actividad2" element={<Actividad2 />}></Route>
-            <Route path="actividad3" element={<Actividad3 />}></Route>
-            <Route path="actividad4" element={<Actividad4 />}></Route>
-            <Route path="actividad5" element={<Actividad5 />}></Route>
-            <Route path="actividad6" element={<Actividad6 />}></Route>
-            <Route
-              path="*"
-              element={
-                <div className="m-5 p-5">
-                  <div className="fs-1 text-center mt-5 pt-5">
-                    404 - Página no encontrada
-                  </div>{" "}
-                </div>
-              }
-            ></Route>
-          </Routes>
-        </div>
-        <div>
-          <Footer />
-        </div>
-      </BrowserRouter>
-    </QueryClientProvider>
+          <div>
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="actividad" element={<Actividad />}></Route>
+              <Route path="media" element={<Media />}></Route>
+              <Route path="misionVision" element={<MisionVision />}></Route>
+              <Route path="queCreemos" element={<QueCreemos />}></Route>
+              <Route path="jesus" element={<Jesus />}></Route>
+              <Route path="pastores" element={<Pastores />}></Route>
+              <Route path="contacto" element={<Contacto />}></Route>
+              <Route path="dar" element={<Dar />}></Route>
+              <Route
+                path="actividad/actividad1"
+                element={<Actividad1 />}
+              ></Route>
+              <Route
+                path="actividad/actividad2"
+                element={<Actividad2 />}
+              ></Route>
+              <Route
+                path="actividad/actividad3"
+                element={<Actividad3 />}
+              ></Route>
+              <Route
+                path="actividad/actividad4"
+                element={<Actividad4 />}
+              ></Route>
+              <Route
+                path="actividad/actividad5"
+                element={<Actividad5 />}
+              ></Route>
+              <Route
+                path="actividad/actividad6"
+                element={<Actividad6 />}
+              ></Route>
+              <Route path="actividad1" element={<Actividad1 />}></Route>
+              <Route path="actividad2" element={<Actividad2 />}></Route>
+              <Route path="actividad3" element={<Actividad3 />}></Route>
+              <Route path="actividad4" element={<Actividad4 />}></Route>
+              <Route path="actividad5" element={<Actividad5 />}></Route>
+              <Route path="actividad6" element={<Actividad6 />}></Route>
+              <Route
+                path="*"
+                element={
+                  <div className="m-5 p-5">
+                    <div className="fs-1 text-center mt-5 pt-5">
+                      404 - Página no encontrada
+                    </div>{" "}
+                  </div>
+                }
+              ></Route>
+            </Routes>
+          </div>
+          <div>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </>
   );
 }
--- src/componentes/spinner/spinner.jsx
+++ src/componentes/spinner/spinner.jsx
@@ -2,7 +2,7 @@ function spinner() {
   return (
     <div
       id="spinner"
-      className="show bg-white position-fixed translate-middle w-100 vh-100 top-80 start-50 d-flex align-items-center justify-content-center"
+      className="show bg-white position-fixed translate-middle w-100 start-50 d-flex align-items-center justify-content-center"
     >
       <div className="spinner-grow text-primary" role="status"></div>
     </div>
